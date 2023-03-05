6e1cf0990e04f65ab1bb0e2edae4c8502294dcf3
ZiongaB <91677985+ZiongaB@users.noreply.github.com>
2023-03-05 14:35:41 -0500

    Navbar
    Navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Test1Component } from './components/test1/test1.component';
+import { Test2Component } from './components/test2/test2.component';
+import { Test3Component } from './components/test3/test3.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
